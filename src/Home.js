a924737ab9b575034c008386624cd5c4e71e9e02
rebecca505 <rlaurelfrey@gmail.com>
2025-08-25 02:26:59 -0700

    allowed users to download files

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,6 @@
-import { AbsoluteCenter, Box, Button } from "@chakra-ui/react";
-import { Octokit } from "@octokit/core";
+import { AbsoluteCenter, Box, Button, DownloadTrigger } from "@chakra-ui/react";
 
 export default function Home() {
-
-    const octokit = new Octokit({ auth: "token" });
-
-    async function runWorkflow() {
-        try {
-            await octokit.request(
-                "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
-                {
-                    owner: "rebecca505",
-                    repo: "instagram-followers-check",
-                    workflow_id: "run.yml",  // workflow file name or workflow id number
-                    ref: "main",
-                    headers: {
-                        "X-GitHub-Api-Version": "2022-11-28",
-                    },
-                }
-            );
-            console.log("Workflow triggered!");
-        } catch (error) {
-            console.error("Failed to trigger workflow:", error);
-        }
-    }
-
     return (
         <div>
             <Box m="5vh" p="5" textAlign="center">
@@ -36,16 +12,27 @@ export default function Home() {
                     instagram scraper
                 </p>
             </Box>
-            <AbsoluteCenter axis="horizontal">
+            <AbsoluteCenter axis="horizontal" gap="50px">
                 <Button
-                    borderColor="black"
+                    borderColor="#dd2a7b"
                     w={"200px"}
                     fontSize={17}
-                    background="#dd2a7b"
+                    background="#f58529"
                     color="#515bd4"
                     _hover={{ bg: "#feda77", borderWidth: "2px" }}
                     borderWidth=".5px">
-                    <a onClick={runWorkflow}>RUN SCRAPER</a>
+                    <a href={require('./files/instagram-data.txt')} download="instagram-data.py">PYTHON FILE</a>
+                </Button>
+                
+                <Button
+                    borderColor="#dd2a7b"
+                    w={"200px"}
+                    fontSize={17}
+                    background="#f58529"
+                    color="#515bd4"
+                    _hover={{ bg: "#feda77", borderWidth: "2px" }}
+                    borderWidth=".5px">
+                    <a href={require('./files/scrape.txt')} download="scrape.js">JAVASCRIPT FILE</a>
                 </Button></AbsoluteCenter>
         </div>
     );
