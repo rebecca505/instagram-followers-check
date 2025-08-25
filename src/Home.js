ae023edd0504f723b0fa29f96facd2f90aff2776
rebecca505 <rlaurelfrey@gmail.com>
2025-08-25 01:18:37 -0700

    button triggers github actions

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,30 @@
-import { Box } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Button } from "@chakra-ui/react";
+import { Octokit } from "@octokit/core";
 
 export default function Home() {
+
+    const octokit = new Octokit({ auth: "token" });
+
+    async function runWorkflow() {
+        try {
+            await octokit.request(
+                "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+                {
+                    owner: "rebecca505",
+                    repo: "instagram-followers-check",
+                    workflow_id: "run.yml",  // workflow file name or workflow id number
+                    ref: "main",
+                    headers: {
+                        "X-GitHub-Api-Version": "2022-11-28",
+                    },
+                }
+            );
+            console.log("Workflow triggered!");
+        } catch (error) {
+            console.error("Failed to trigger workflow:", error);
+        }
+    }
+
     return (
         <div>
             <Box m="5vh" p="5" textAlign="center">
@@ -12,6 +36,17 @@ export default function Home() {
                     instagram scraper
                 </p>
             </Box>
+            <AbsoluteCenter axis="horizontal">
+                <Button
+                    borderColor="black"
+                    w={"200px"}
+                    fontSize={17}
+                    background="#dd2a7b"
+                    color="#515bd4"
+                    _hover={{ bg: "#feda77", borderWidth: "2px" }}
+                    borderWidth=".5px">
+                    <a onClick={runWorkflow}>RUN SCRAPER</a>
+                </Button></AbsoluteCenter>
         </div>
     );
 }
