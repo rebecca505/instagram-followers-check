c4a0c626c316c7c616530afb861d1797ae5f322c
rebecca505 <rlaurelfrey@gmail.com>
2025-08-25 23:22:47 -0700

    pull and save followers

---
+++ scrape.js
@@ -0,0 +1,58 @@
+
+
+(async () => {
+    async function scrapeData() {
+        const username = "username";
+
+        let followers = [];
+        try {
+            console.log(`Process started! Give it a couple of seconds`);
+
+            const userQueryRes = await fetch(
+                `https://www.instagram.com/web/search/topsearch/?query=${username}`
+            );
+
+            const userQueryJson = await userQueryRes.json();
+
+            const userId = userQueryJson.users
+                .map(u => u.user)
+                .filter(u => u.username === username)[0].pk;
+
+            let after = null;
+            let has_next = true;
+
+            while (has_next) {
+                await fetch(
+                    `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=` +
+                    encodeURIComponent(
+                        JSON.stringify({
+                            id: userId,
+                            include_reel: true,
+                            fetch_mutual: true,
+                            first: 50,
+                            after: after,
+                        })
+                    )
+                )
+                    .then((res) => res.json())
+                    .then((res) => {
+                        has_next = res.data.user.edge_followed_by.page_info.has_next_page;
+                        after = res.data.user.edge_followed_by.page_info.end_cursor;
+                        followers = followers.concat(
+                            res.data.user.edge_followed_by.edges.map(({ node }) => ({
+                                username: node.username,
+                                full_name: node.full_name,
+                            }))
+                        );
+                    });
+            }
+
+            console.log(`scraped followers`);
+            return { followers };
+
+        } catch (err) {
+            return { error: err.toString() };
+        }
+    }
+    return await scrapeData();
+})();
